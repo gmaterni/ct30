93c205207b476d0830137db1443fca5e8c09c5d5
giuseppe materni <giuseppe.materni@gmail.com>
2026-05-31 00:58:27 +0200

    31-05-26 (00:58)

---
+++ aider.sh
@@ -0,0 +1,7 @@
+
+ 
+  aider \
+    --input-history-file  .aider/.aider.input.history \
+    --chat-history-file   .aider/.aider.chat.history.md \
+    --llm-history-file    .aider/.aider.llm.history \
+    "$@"  
